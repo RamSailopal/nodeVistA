9380100f4bf914e78dbc460b971419e986bc73a7
Raman Sailopal <thelinuxguys@yahoo.com>
2022-11-22 10:08:50 +0000

    Delete index.js
    Not needed

--- Microservices/nodem/index.js
+++
@@ -1,5 +0,0 @@
-const ydb = require('nodem').Ydb();
-ydb.open({ipAddress: 'nodevista', tcpPort: 6789});
-ydb.set('^RAM', 1, 'This is a test')
-var resp=ydb.get({global: 'RAM', subscripts: [1]})
-console.log(resp)
